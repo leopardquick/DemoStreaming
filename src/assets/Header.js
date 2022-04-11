4be4fad692e8c32154edcdd8b8671bbf34f0999a
unknown <nadirhemed@gmail.com>
2022-04-11 10:36:13 +0300

    Design change

--- src/assets/Header.js
+++ src/assets/Header.js
@@ -5,11 +5,19 @@ import {Link} from 'react-router-dom'
 
 let Header = () => {
     return (
-    <nav className="navbar navbar-dark bg-primary customColor">
-        <div className="container-fluid">
-         <h2> <a className="navbar-brand" href="/">Demo Streaming</a> </h2>
-        </div>
-      </nav>
+    <nav>
+      <ul className="homenav-list">
+        <li className="homenav-item">
+          <a href="/">Demo Streaming</a>
+        </li>
+        <li className="homenav-item">
+          <a href="#">Login</a>
+        </li>
+        <li className="homenav-item">
+          <button className="homebtn-primary">Start your free trial</button>
+        </li>
+      </ul>
+    </nav>
     )
 }
 
--- src/assets/HomeCard.js
+++ src/assets/HomeCard.js
@@ -3,13 +3,37 @@ import {Link} from 'react-router-dom'
 
 export default function HomeCard(){
     return (
-        <div  className="boxContainer">
-          <Link className="Homebox" to={"/movie"}>
-            <center>movie</center>
-          </Link>
-          <Link className="Homebox" to={"/series"}>
-            <center>series</center>
-          </Link>
+        // <div  className="boxContainer">
+        //   <Link className="Homebox" to={"/movie"}>
+        //     <center>movie</center>
+        //   </Link>
+        //   <Link className="Homebox" to={"/series"}>
+        //     <center>series</center>
+        //   </Link>
+        // </div>
+
+        <div className="homeContainer">
+          <div className="homeCard">
+            <div className="homeBox">
+              <div className="homeContent">
+                <h2>01</h2>
+                <h3>Movies</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <a href="/movie">Watch</a>
+              </div>
+            </div>
+          </div>
+          <div className="homeCard">
+            <div className="homeBox">
+              <div className="homeContent">
+                <h2>02</h2>
+                <h3>Series</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <a href="/series">Watch</a>
+              </div>
+            </div>
+          </div>
+          
         </div>
     )
 }
